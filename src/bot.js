a30fe28c6c38533a90ea348bd3f3786d8df6211f
isabel <isabelroseslive@gmail.com>
2023-03-23 12:23:35 +0000

    fallback

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,9 @@
 require('dotenv').config();
-const { Client, Collection, Partials, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildMessageReactions
-    ],
-    partials: [
-        Partials.Channel,
-        Partials.Message,
-        Partials.Reaction
-    ],
-});
+const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
 client.commandArray = [];
 client.buttons = new Collection();
