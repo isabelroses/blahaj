c7e2299f737a024ccfb193d43e9bdd8c445d9cd8
isabel <isabel@isabelroses.com>
2024-06-09 20:30:17 +0100

    feat: code expantion

--- src/event_handler/mod.rs
+++ src/event_handler/mod.rs
@@ -1,13 +1,17 @@
 use color_eyre::eyre::Result;
 use poise::serenity_prelude::{Context, FullEvent};
 
+mod code_expantion;
+
 use crate::Data;
 
-// TODO: add more events
-pub async fn event_handler(_ctx: &Context, event: &FullEvent, _data: &Data) -> Result<()> {
+// TODO: add starboard
+pub async fn event_handler(ctx: &Context, event: &FullEvent, _data: &Data) -> Result<()> {
     if let FullEvent::Ready { data_about_bot } = event {
         println!("Logged in as {}", data_about_bot.user.name);
     }
+
+    code_expantion::handle(ctx, event).await?;
 
     Ok(())
 }
