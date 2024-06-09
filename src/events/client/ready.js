691ff934ac416ce6368150c735f719be8ff1afe0
isabel <isabel@isabelroses.com>
2024-06-09 09:02:37 +0100

    feat!: delete the old

--- src/events/client/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        console.log(`Logged in as ${client.user.tag}!`);
-        client.handleCommands();
-        client.pickPresence();
-    }
-};
