bc96896f974afbbffc3d54138eb2002273eef73f
robin <67917529+comfysage@users.noreply.github.com>
2024-06-10 15:38:18 +0200

    feat: roll command (#2)
    Co-authored-by: Isabel <isabel@isabelroses.com>

---
+++ src/commands/dice.rs
@@ -0,0 +1,15 @@
+use crate::Context;
+use color_eyre::eyre::Result;
+use rand::Rng;
+
+/// Rolls dice based on given # of sides
+#[poise::command(slash_command)]
+pub async fn roll(
+    ctx: Context<'_>,
+    #[description = "# of sides"] sides: Option<u32>,
+) -> Result<()> {
+    let sides = sides.unwrap_or(6);
+    let roll = rand::thread_rng().gen_range(1..=sides);
+    ctx.say(format!("You rolled a **{}**", roll)).await?;
+    Ok(())
+}
