3c2bd7ee788429daa623e7ddde540802d314f85d
isabel <isabel@isabelroses.com>
2024-07-08 09:44:03 +0100

    chore: cleanup code expantion

--- src/event_handler/code_expantion.rs
+++ src/event_handler/code_expantion.rs
@@ -1,21 +1,19 @@
 // the logic here is pretty much ripped from https://github.com/uncenter/discord-forum-bot/blob/main/src/modules/expandGitHubLinks.ts
 // with some modifications so I can make it work on diffrent git hosts
 
-use color_eyre::eyre::Result;
-use poise::serenity_prelude::{self as serenity, Context, CreateMessage, FullEvent};
+use color_eyre::eyre::{self, Result};
+use poise::serenity_prelude::{Context, FullEvent};
 use regex::Regex;
 use reqwest::Client;
 
 pub async fn handle(ctx: &Context, event: &FullEvent) -> Result<()> {
     if let FullEvent::Message { new_message } = event {
-        let mut embeds: Vec<serenity::CreateEmbed> = Vec::new();
+        let code_blocks = extract_code_blocks(new_message.content.clone()).await?;
 
-        embeds.extend(github_compatable_embeds(new_message.content.clone()).await?);
-
-        if !embeds.is_empty() {
+        if !code_blocks.is_empty() {
             new_message
                 .channel_id
-                .send_message(&ctx, CreateMessage::default().embeds(embeds))
+                .say(ctx, code_blocks.join("\n"))
                 .await?;
         }
     }
@@ -23,110 +21,96 @@ pub async fn handle(ctx: &Context, event: &FullEvent) -> Result<()> {
     Ok(())
 }
 
-async fn github_compatable_embeds(msg: String) -> Result<Vec<serenity::CreateEmbed>> {
+async fn extract_code_blocks(msg: String) -> Result<Vec<String>> {
     let re = Regex::new(
-        r"https?://(?P<host>(git.*|codeberg\.org))/(?P<repo>[\w-]+/[\w.-]+)/(blob|(src/(commit|branch)))?/(?P<reference>\S+?)/(?P<file>\S+)#L(?P<start>\d+)(?:[~-]L?(?P<end>\d+)?)?"
-        ).unwrap();
+        r"https?://(?P<host>(git.*|codeberg\.org))/(?P<repo>[\w-]+/[\w.-]+)/(blob|(src/(commit|branch)))?/(?P<reference>\S+?)/(?P<file>\S+)#L(?P<start>\d+)(?:[~-]L?(?P<end>\d+)?)?",
+    )?;
 
-    let mut embeds: Vec<serenity::CreateEmbed> = Vec::new();
+    let mut blocks: Vec<String> = Vec::new();
+    let client = Client::new();
 
-    let mut code_blocks = Vec::new();
     for caps in re.captures_iter(&msg) {
-        let host = &caps["host"];
-        let repo = &caps["repo"];
-        let reference = &caps["reference"];
-        let file = &caps["file"];
-        let language = file.split('.').last().unwrap_or("").to_string();
+        let (host, repo, reference, file, start, end) = extract_url_components(&caps)?;
 
-        let start = caps["start"].parse::<usize>().unwrap_or(1);
-        let end = caps
-            .name("end")
-            .and_then(|end| end.as_str().parse::<usize>().ok())
-            .unwrap_or(start);
+        let raw_url = construct_raw_url(host, repo, reference, file);
 
-        let raw_url = if host == "github.com" {
-            format!("https://raw.githubusercontent.com/{repo}/{reference}/{file}")
-        } else {
-            let refer = if reference.len() == 40 {
-                format!("commit/{reference}")
-            } else {
-                format!("branch/{reference}")
-            };
-            format!("https://{host}/{repo}/raw/{refer}/{file}")
-        };
-
-        let response = Client::new().get(&raw_url).send().await;
-
-        if let Ok(response) = response {
-            if !response.status().is_success() {
-                println!("Failed to fetch content.");
-                continue;
-            }
-
-            let text = response.text().await?;
-            let mut content = text
-                .lines()
-                .skip(start - 1)
-                .take(end - start + 1)
-                .collect::<Vec<&str>>()
-                .join("\n");
-
-            let mut lines_sliced = 0;
-
-            while content.len() > 1950 {
-                let lines = content.lines().collect::<Vec<&str>>();
-                if lines.len() == 1 {
-                    content = content.chars().take(1950).collect();
-                    break;
-                }
-                content = lines[..lines.len() - 1].join("\n");
-                lines_sliced += 1;
-            }
-            let end = end - lines_sliced;
-
-            let name = format!(
-                "{repo}@{} {file} L{start}{}",
-                if reference.len() == 40 {
-                    &reference[..8]
-                } else {
-                    &reference
-                },
-                if end > start {
-                    format!("-{end}")
-                } else {
-                    String::new()
-                }
-            );
-
-            let body = if lines_sliced > 0 {
-                format!("... ({lines_sliced} lines not displayed)")
-            } else {
-                String::new()
-            };
-
-            code_blocks.push((name, language, content, body));
-        }
-
-        code_blocks.retain(|(_, _, content, _)| !content.trim().is_empty());
-
-        if !code_blocks.is_empty() {
-            embeds.extend(generic_codeblocks_to_embed(&code_blocks));
+        if let Ok(code_block) = fetch_code_block(&client, &raw_url, start, end, file).await {
+            blocks.push(code_block);
         }
     }
 
-    Ok(embeds)
+    Ok(blocks)
 }
 
-type CodeBlock = (String, String, String, String);
+fn extract_url_components<'a>(
+    caps: &'a regex::Captures<'a>,
+) -> Result<(&'a str, &'a str, &'a str, &'a str, usize, usize)> {
+    let host = &caps["host"];
+    let repo = &caps["repo"];
+    let reference = &caps["reference"];
+    let file = &caps["file"];
+    let start = caps["start"].parse::<usize>()?;
+    let end = caps
+        .name("end")
+        .map_or(Ok(start), |end| end.as_str().parse::<usize>())?;
 
-fn generic_codeblocks_to_embed(codeblocks: &[CodeBlock]) -> Vec<serenity::CreateEmbed> {
-    codeblocks
-        .iter()
-        .map(|(name, language, content, body)| {
-            serenity::CreateEmbed::default()
-                .title(name)
-                .description(format!("```{language}\n{content}\n```").to_owned())
-                .footer(serenity::CreateEmbedFooter::new(body))
-        })
-        .collect::<Vec<_>>()
+    Ok((host, repo, reference, file, start, end))
+}
+
+fn construct_raw_url(host: &str, repo: &str, reference: &str, file: &str) -> String {
+    if host == "github.com" {
+        format!("https://raw.githubusercontent.com/{repo}/{reference}/{file}")
+    } else {
+        let refer = if reference.len() == 40 {
+            format!("commit/{reference}")
+        } else {
+            format!("branch/{reference}")
+        };
+        format!("https://{host}/{repo}/raw/{refer}/{file}")
+    }
+}
+
+async fn fetch_code_block(
+    client: &Client,
+    raw_url: &str,
+    start: usize,
+    end: usize,
+    file: &str,
+) -> Result<String> {
+    let response = client.get(raw_url).send().await?;
+    if !response.status().is_success() {
+        return Err(eyre::eyre!("Failed to fetch content from {}", raw_url));
+    }
+
+    let text = response.text().await?;
+    let content = text
+        .lines()
+        .skip(start - 1)
+        .take(end - start + 1)
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    let language = file
+        .split('.')
+        .last()
+        .map_or("", remove_query_string)
+        .to_lowercase();
+
+    Ok(format_code_block(language, content))
+}
+
+fn format_code_block(language: String, content: String) -> String {
+    if content.len() > 1950 {
+        let truncated_content = content.lines().take(1950).collect::<Vec<&str>>().join("\n");
+        format!(
+            "```{}\n{}\n```\n... (lines not displayed)",
+            language, truncated_content
+        )
+    } else {
+        format!("```{}\n{}\n```", language, content)
+    }
+}
+
+fn remove_query_string(input: &str) -> &str {
+    input.split('?').next().unwrap_or(input)
 }
