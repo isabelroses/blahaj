4c6b2265add5b108d0a48c3e87a84c2ed4ac7983
isabel <isabel@isabelroses.com>
2024-10-13 15:42:19 +0100

    refactor: update for new vers

--- src/commands/misc/nixpkgs.rs
+++ src/commands/misc/nixpkgs.rs
@@ -1,5 +1,5 @@
 use color_eyre::eyre::Result;
-use git_tracker::Tracker;
+use git_tracker::TrackedRepository;
 use poise::{serenity_prelude::CreateEmbed, CreateReply};
 use serde::Deserialize;
 use std::env;
@@ -20,7 +20,11 @@ pub async fn nixpkgs(
 
     let nixpkgs_path = env::var("NIXPKGS").expect("NIXPKGS not set");
     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
-    let tracker = Tracker::from_path(&nixpkgs_path)?;
+    let tracker = TrackedRepository::new(
+        nixpkgs_path.into(),
+        NIXPKGS_URL.to_string(),
+        "origin".to_string(),
+    );
 
     let client = &ctx.data().client;
 
@@ -44,31 +48,28 @@ pub async fn nixpkgs(
         return Ok(());
     };
 
-    let mut status_results = vec![];
-    for branch_name in &[
-        "master",
-        "staging",
-        "nixpkgs-unstable",
-        "nixos-unstable-small",
-        "nixos-unstable",
-    ] {
-        let full_branch_name = format!("origin/{branch_name}");
-        let has_pr = tracker.branch_contains_sha(&full_branch_name, &commit_sha)?;
+    let branchs = vec![
+        "master".to_string(),
+        "staging".to_string(),
+        "nixpkgs-unstable".to_string(),
+        "nixos-unstable-small".to_string(),
+        "nixos-unstable".to_string(),
+    ];
 
-        if has_pr {
-            status_results.push(format!("`{branch_name}` ✅"));
-        } else {
-            status_results.push(format!("`{branch_name}` ❌"));
-        }
-    }
+    let prd_branches = tracker.branches_contain_sha(&branchs, &commit_sha)?;
 
     // if we don't find the commit in any branches from above, we can pretty safely assume
     // it's an unmerged PR
-    let embed_description: String = if status_results.is_empty() {
+    let embed_description: String = if prd_branches.is_empty() {
         "It doesn't look like this PR has been merged yet! (or maybe I just haven't updated)"
             .to_string()
     } else {
-        status_results.join("\n")
+        prd_branches
+            .iter()
+            .fold(String::new(), |acc, (name, has_commit)| {
+                let emoji = if *has_commit { "✅" } else { "❌" };
+                format!("{acc}\n{name} {emoji}")
+            })
     };
 
     let embed = CreateReply::default().embed(
