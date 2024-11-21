19436e906ed84890e7515b43ad41ae6ef8c377cf
isabel <isabel@isabelroses.com>
2024-11-21 21:59:31 +0000

    refactor(commands/nixpkgs): use nixpkgs-track for the backend

--- src/commands/misc/nixpkgs.rs
+++ src/commands/misc/nixpkgs.rs
@@ -1,12 +1,17 @@
 use color_eyre::eyre::Result;
-use git_tracker::TrackedRepository;
+use nixpkgs_track::{branch_contains_commit, fetch_nixpkgs_pull_request};
 use poise::{serenity_prelude::CreateEmbed, CreateReply};
-use serde::Deserialize;
 use std::env;
 
 use crate::Context;
 
-const NIXPKGS_URL: &str = "https://github.com/NixOS/nixpkgs";
+const BRANCHES: [&str; 5] = [
+    "master",
+    "staging",
+    "nixpkgs-unstable",
+    "nixos-unstable-small",
+    "nixos-unstable",
+];
 
 /// Track nixpkgs PRs
 #[poise::command(slash_command)]
@@ -14,76 +19,48 @@ pub async fn nixpkgs(
     ctx: Context<'_>,
     #[description = "pr"]
     #[min = 0]
-    pr: i32,
+    pr: u64,
 ) -> Result<()> {
     ctx.defer().await?;
 
-    let nixpkgs_path = env::var("NIXPKGS").expect("NIXPKGS not set");
     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
-    let tracker = TrackedRepository::new(
-        nixpkgs_path.into(),
-        NIXPKGS_URL.to_string(),
-        "origin".to_string(),
-    );
 
-    let client = &ctx.data().client;
+    let pull_request = tokio::task::spawn_blocking({
+        let github_token = github_token.clone();
+        move || fetch_nixpkgs_pull_request(pr, Some(&github_token))
+    })
+    .await??;
 
-    // find out what commit our PR was merged in
-    let Some(commit_sha) = ({
-        let url = format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{pr}");
-        let resp = client
-            .get(&url)
-            .header("User-Agent", "blahaj")
-            .header("Authorization", format!("Bearer {github_token}"))
-            .send()
-            .await
-            .expect("error fetching")
-            .json::<PullRequest>()
-            .await?;
-
-        resp.merge_commit_sha
-    }) else {
-        ctx.say("It seems this pull request is very old. I can't track it")
-            .await?;
+    let Some(commit_sha) = pull_request.merge_commit_sha else {
+        println!("This pull request is very old. I can't track it!");
         return Ok(());
     };
 
-    let branchs = vec![
-        "master".to_string(),
-        "staging".to_string(),
-        "nixpkgs-unstable".to_string(),
-        "nixos-unstable-small".to_string(),
-        "nixos-unstable".to_string(),
-    ];
+    let mut embed_description = String::new();
+    for branch in BRANCHES {
+        let github_token = github_token.clone();
+        let commit_sha = commit_sha.clone();
 
-    let prd_branches = tracker.branches_contain_sha(&branchs, &commit_sha)?;
+        let has_pull_request = tokio::task::spawn_blocking(move || {
+            branch_contains_commit(branch, &commit_sha, Some(&github_token))
+        })
+        .await
+        .unwrap_or(Ok(false))?;
 
-    // if we don't find the commit in any branches from above, we can pretty safely assume
-    // it's an unmerged PR
-    let embed_description: String = if prd_branches.is_empty() {
-        "It doesn't look like this PR has been merged yet! (or maybe I just haven't updated)"
-            .to_string()
-    } else {
-        prd_branches
-            .iter()
-            .fold(String::new(), |acc, (name, has_commit)| {
-                let emoji = if *has_commit { "✅" } else { "❌" };
-                format!("{acc}\n{name} {emoji}")
-            })
-    };
+        embed_description.push_str(&format!(
+            "{}: {}\n",
+            branch,
+            if has_pull_request { "✅" } else { "❌" }
+        ));
+    }
 
     let embed = CreateReply::default().embed(
         CreateEmbed::new()
-            .title(format!("Nixpkgs PR #{pr} Status"))
-            .url(format!("{NIXPKGS_URL}/pull/{pr}"))
+            .title(format!("{} - #{}", pull_request.title, pull_request.number))
+            .url(pull_request.html_url)
             .description(embed_description),
     );
 
     ctx.send(embed).await?;
     Ok(())
-}
-
-#[derive(Clone, Debug, Deserialize)]
-struct PullRequest {
-    merge_commit_sha: Option<String>,
 }
