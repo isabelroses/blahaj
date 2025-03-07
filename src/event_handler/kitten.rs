c6d692e13294d38b7c1eaa6603660b4554b773e0
isabel <isabel@isabelroses.com>
2025-03-07 18:19:49 +0000

    refactor: cleanup joining and role managment

--- src/event_handler/kitten.rs
+++ src/event_handler/kitten.rs
@@ -1,75 +1,46 @@
 use color_eyre::eyre::{eyre, Result};
 use poise::serenity_prelude::{Context, FullEvent};
 use reqwest::Client;
-use serenity::all::{ChannelId, Member, PartialMember, RoleId, UserId};
+use serenity::all::{ChannelId, Member, RoleId, UserId};
 
 pub async fn handle(ctx: &Context, event: &FullEvent, _client: &Client) -> Result<()> {
     if let FullEvent::GuildMemberAddition { new_member } = event {
+        if new_member.guild_id != 1095080242219073606 {
+            return Ok(());
+        }
+
+        if new_member.user.bot {
+            return Ok(());
+        }
+
         warn_onboarding(ctx, &new_member.user.id).await?;
-    } else if let FullEvent::GuildMemberUpdate {
+    }
+
+    if let FullEvent::GuildMemberUpdate {
         old_if_available: _,
-        new,
+        new: Some(member),
         event: _,
     } = event
     {
-        if let Some(member) = new {
-            if member.user.bot {
-                return Ok(());
-            }
-            // check if the user does not have the kitten role
-            if !member.roles.iter().any(|role| *role == 1249814690486423612) {
-                // check if the user has the pronouns role
-                if member
-                    .roles
-                    .iter()
-                    .filter(|role| is_pronouns_role(**role))
-                    .count()
-                    > 0
-                {
-                    add_kitten_role(ctx, member).await?;
-                }
-            }
-        }
-    } else if let FullEvent::Message { new_message } = event {
-        let user = &new_message.author;
-        let partial_member: PartialMember = user
-            .member
-            .clone()
-            .ok_or(eyre!("Failed to get member"))?
-            .as_mut()
-            .to_owned();
-        let member = Member::from(partial_member);
         if member.user.bot {
             return Ok(());
         }
-        // check if the user has the kitten role
-        if member.roles.iter().any(|role| *role == 1249814690486423612) {
-            // check if the user does not have pronouns selected
+
+        // check if the user does not have the kitten role
+        if !member.roles.iter().any(|role| *role == 1249814690486423612) {
+            // check if the user has the pronouns role
             if member
                 .roles
                 .iter()
                 .filter(|role| is_pronouns_role(**role))
                 .count()
-                == 0
+                > 0
             {
-                // delete the message
-                new_message
-                    .delete(ctx)
-                    .await
-                    .map_err(|e| eyre!("Failed to delete message: {}", e))?;
-
-                // send a message
-                warn_onboarding(ctx, &user.id).await?;
-
-                // remove the kitten role
-
-                member
-                    .remove_role(ctx, 1249814690486423612)
-                    .await
-                    .map_err(|e| eyre!("Failed to remove role: {}", e))?;
+                add_kitten_role(ctx, member).await?;
             }
         }
     }
+
     Ok(())
 }
 
@@ -82,6 +53,7 @@ async fn warn_onboarding(ctx: &Context, user_id: &UserId) -> Result<(), color_ey
             ),
         )
         .await?;
+
     Ok(())
 }
 
